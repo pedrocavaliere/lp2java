588b3d96daecd80d179929c5a245422892ccf857
Ronnie Paskin <ronnie@megaputz.com>
2014-03-11 12:17:08 -0300

    Primeiro demo

---
+++ Party.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.event.*;
+class Party {
+	public void buildInvite(){
+		Frame f = new Frame();
+		Label l = new Label("Festa na casa do Tim!");
+		Button b = new Button("Com certeza!");
+		Button c = new Button("Nem pensar");
+		Panel p = new Panel();
+		p.add(l);
+	}
+}
