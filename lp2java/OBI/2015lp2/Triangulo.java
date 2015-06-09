3e2e84bca82076af5b0148ea3df7596f762ae575
rpaskin <ronnie@megaputz.com>
2015-06-09 16:29:27 -0300

    Criando arquivos triangulo OBI 2014

---
+++ lp2java/OBI/2015lp2/Triangulo.java
@@ -0,0 +1,17 @@
+class Triangulo {
+  int g1;
+  int g2;
+  int g3;
+  int g4;
+
+  void setG(int a, int b, int c, int d){
+    this.g1 = a;
+    this.g2 = b;
+    this.g3 = c;
+    this.g4 = d;
+  }
+
+  boolean checarTriangulo(){
+    return false;
+  }
+}
---
+++ lp2java/OBI/2015lp2/TrianguloTestDrive.java
@@ -0,0 +1,8 @@
+class TrianguloTestDrive{
+  public static void main(String[] args) {
+    Triangulo t = new Triangulo();
+      t.setG(10,8,9,7);
+      System.out.println(t.checarTriangulo());
+
+   } 
+}
