7595e5f3a15ea05a221a6445f9206f6edac4980f
rpaskin <ronnie@megaputz.com>
2015-06-09 16:52:02 -0300

    Colocamos mais testes no TestDrive

--- lp2java/OBI/2015lp2/TrianguloTestDrive.java
+++ lp2java/OBI/2015lp2/TrianguloTestDrive.java
@@ -1,10 +1,28 @@
 class TrianguloTestDrive{
   public static void main(String[] args) {
     Triangulo t = new Triangulo();
-      t.setG(10,8,9,7);
+      t.setG(6,9,22,5);
       System.out.println(t.checarTriangulo());
-      
+
       t.setG(14,40,12,60);
       System.out.println(t.checarTriangulo());
+
+      t.setG(6,9,5,100);
+      System.out.println(t.checarTriangulo());
+
+      t.setG(41,98,38,51);
+      System.out.println(t.checarTriangulo());
+
+      t.setG(59,8,40,1);
+      System.out.println(t.checarTriangulo());
+
+
+// 6 9 22 5
+// 14 40 12 60
+// 6 9 5 100
+// 41 98 38 51
+// 59 8 40 1
+
+
    } 
 }
